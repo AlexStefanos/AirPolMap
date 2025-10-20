91b6be7eebfc8add0036324dde044b791a3d9bdd
AlexStefanos <alexandrestefanoss@gmail.com>
2025-10-20 15:12:09 +0200

    Changing display for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 /// <reference types="vite-plugin-svgr/client" />
 
-import "./App.css"
-import FranceRegions from "./assets/france.regions.svg?react"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import FranceRegions from './assets/france.regions.svg?react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
         <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
           <FranceRegions />
         </div>
         <p className="read-the-docs">
