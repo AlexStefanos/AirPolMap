9b7e872cc0157de71de7b4c59542f7738bcee607
AlexStefanos <alexandrestefanoss@gmail.com>
2025-12-08 11:44:50 +0100

    First commit of the end of the project : New DatePicker and new Selecter

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,45 @@
 
 import './App.css'
 import FranceRegions from './assets/france.regions.svg?react'
+import Pikaday from "pikaday";
+import { useEffect, useRef } from "react";
+import "pikaday/css/pikaday.css";
 
 function App() {
+  const pickerRef = useRef(null);
+
+  useEffect(() => {
+    const picker = new Pikaday({
+      field: pickerRef.current,
+      format: 'DD/MM/YYYY',
+    });
+
+    return () => picker.destroy();
+  }, []);
+
   return (
     <>
-        <div>
-          <FranceRegions />
+        <div style={{ marginBottom: "30px", display: "flex", alignItems: "center", gap: "100px"}}>
+          <input 
+            ref={pickerRef}
+            type="text"
+            className="input pika-single"
+            placeholder="Choisissez une date"
+          />
+          <fieldset>
+            <legend>Polluant choisi</legend>
+            <select defaultValue="Choisissez un polluant">
+              <option>Dioxyde de soufre (SO2)</option>
+              <option>Dioxyde d’azote (NO2)</option>
+              <option>Monoxyde de carbone (CO)</option>
+              <option>Ozone (O3)</option>
+              <option>Oxyde d’azote (NOx)</option>
+            </select>
+          </fieldset>
         </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
+        <div>
+          <FranceRegions style={{ marginLeft:"-100px", width: "160%"}} />
+        </div>
     </>
   )
 }
